cbdd7f313b3a51ffac65af7d2bf3a4b10d250d25
xementor <ahammedzonaid@gmail.com>
2022-05-23 10:54:51 +0600

    day4-p2-iterative solution

--- leetcode/94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.java
+++ leetcode/94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.java
@@ -18,13 +18,14 @@
     - go depth of left
     - then go right
 */
-class Solution {
+class Solution2 {
     List<Integer> result = new ArrayList<Integer>();
     public List<Integer> inorderTraversal(TreeNode root) {
         traverse(root);
         return result;
         
     }
+    
     void traverse(TreeNode root){
         if(root != null){
             inorderTraversal(root.left);
@@ -33,4 +34,33 @@ class Solution {
         }
         
     }
+}
+
+/**
+    Aproach 2: DFS with iteration ( while loop )
+    - make a Deque Stack.
+    -2 add all the left element in stack
+    - remove one , add this in result -> go its right
+    - then repeat from 2
+    - 
+*/
+class Solution{
+    
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<Integer>();
+        Deque<TreeNode> stack = new ArrayDeque<TreeNode>();
+        TreeNode head = root;
+        
+        while (!stack.isEmpty() || head != null){
+            while (head != null){
+                stack.add(head);
+                head = head.left;
+            }
+            head = stack.removeLast();
+            result.add(head.val);
+            head = head.right;
+        }
+        return result;
+    }
+    
 }
